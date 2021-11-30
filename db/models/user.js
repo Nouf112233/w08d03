df9b1b1a79ea3883c89a78a37b80f4bfc8217d72
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-30 13:18:11 +0300

    edit user db

--- db/models/user.js
+++ db/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const user = new mongoose.Schema({
   email:{type: String, required: true ,unique:true},
   password:{type: String, required: true },
-  role: {type: mongoose.Schema.Types.ObjectId, ref: "Role", required: true }
+  role: {type: mongoose.Schema.Types.ObjectId, ref: "Role", required: true },
+  task: {type: mongoose.Schema.Types.ObjectId, ref: "Task"}
 
  
 });
