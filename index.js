b667f18a5a8a2362b87a40d486e4304b022f15bc
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-30 12:57:27 +0300

    create index.js and db and routers

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("./db");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const roleRouter = require("./routers/routes/role");
+app.use("/role", roleRouter);
+
+const userRouter = require("./routers/routes/user");
+app.use(userRouter);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
---
+++ routers/controllers/user.js
@@ -0,0 +1,62 @@
+const userModel = require("./../../db/models/user");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const SALT = Number(process.env.SALT);
+const jwt = require("jsonwebtoken");
+const secret = process.env.secretKey;
+
+const register = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  const savedEmail = email.toLowerCase();
+  const savedPassword = await bcrypt.hash(password, SALT);
+
+  const newUser = new userModel({
+    email: savedEmail,
+    password: savedPassword,
+    role,
+  });
+  newUser
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  const savedEmail = email.toLowerCase();
+
+  userModel
+    .findOne({ email: savedEmail })
+    .then(async (result) => {
+      if (result) {
+        if (result.email == email) {
+          const hashedPass = await bcrypt.compare(password, result.password);
+          if (hashedPass) {
+            const payload = {
+              role: result.role,
+            };
+            const options = { expiresIn: "60m" };
+            const token = await jwt.sign(payload, secret, options);
+            res.status(200).json({ result, token });
+          } else {
+            res.status(400).json("invalid email or password");
+          }
+        } else {
+          res.status(400).json("invalid email or password");
+        }
+      } else {
+        res.status(404).json("email does not exit");
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = { register, login };
